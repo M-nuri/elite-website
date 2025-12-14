2f0d3f7b166082f055e0f7a34aa8e7a7b5470cef
mf010 <mohammedfurrara1@gmail.com>
2025-12-15 00:14:00 +0200

    changesreact

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react/', // ✅ REQUIRED
   plugins: [react()],
   resolve: {
     alias: {
